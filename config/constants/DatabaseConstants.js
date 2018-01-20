8d290dca06646a7e864213f26ee7dcd7b6cc708a
Christopher DuBois <chdubois@cisco.com>
2018-01-19 18:52:36 -0800

    feat(const)

---
+++ config/constants/DatabaseConstants.js
@@ -0,0 +1,7 @@
+export const CHILD_ADDED = 'child_added';
+export const POST = 'post';
+export const SECTION = 'section';
+export const CHILD_REMOVED = 'child_removed';
+export const CHILD_CHANGED = 'child_changed';
+export const VALUE = 'value';
+export const SLASH = '/';
